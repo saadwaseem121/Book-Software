f614b39132d1a9ae73f0d54c3ff6620d19a1f376
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-02-18 13:48:37 -0500

    Update BookRepository.java
    Added ability to find a book by its ISBN

--- demo/src/main/java/com/example/demo/book/BookRepository.java
+++ demo/src/main/java/com/example/demo/book/BookRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+//    @Query("SELECT b FROM Book b WHERE book.ISBN = ?1")
+    Optional<Book> findBookByISBN(String ISBN);
 }
