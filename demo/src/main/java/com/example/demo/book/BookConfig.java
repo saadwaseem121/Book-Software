51fdce054a10f0314389353bf350776e3bc2c279
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-02-18 13:50:30 -0500

    Update BookConfig.java
    added more books

--- demo/src/main/java/com/example/demo/book/BookConfig.java
+++ demo/src/main/java/com/example/demo/book/BookConfig.java
@@ -12,13 +12,19 @@ public class BookConfig {
     CommandLineRunner bookCommandLineRunner(BookRepository repository){
         return args->
         {
-            Book harry = new Book(747532699L, "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy",
+            Book book1 = new Book("0747532699", "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy",
                         "Harry Potter and the Sorcerer's Stone is an enthralling start to\n" +
                         "  Harry's journey toward coming to terms with his past and facing his future.",
                         "Bloomsbury", 1997, 12.99);
-
+            Book book2 = new Book("0261103342", "The Hobbit", "J.R.R. Tolkien", "High fantasy",
+                    "The critically acclaimed novel that kickstarted " +
+                            "the famous Middle Earth novels by J.R.R. Tolkien.",
+                    "Harper Collins", 1937, 14.99);
+            Book book3 = new Book("0670813028", "It", "Stephen King", "Horror",
+                    "One of Stephen King's most prolific novels, starring the iconic clown character, " +
+                            "Pennywise.", "Viking", 1986, 20.99);
             repository.saveAll(
-                    List.of(harry)
+                    List.of(book1, book2, book3)
             );
         };
     };
