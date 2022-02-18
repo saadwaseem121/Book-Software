e2409b5edf1cfc929dd8a64c5848391ded479b31
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-02-18 13:49:34 -0500

    Update BookService.java
    Added protocol for adding and deleting books from the database

--- demo/src/main/java/com/example/demo/book/BookService.java
+++ demo/src/main/java/com/example/demo/book/BookService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -17,5 +18,23 @@ public class BookService {
 
     public List<Book> getBooks() {
         return bookRepository.findAll();
+    }
+
+    public void addNewBook(Book book) {
+        Optional<Book> bookOptional = bookRepository.findBookByISBN(book.getISBN());
+        if (bookOptional.isPresent())
+        {
+            throw new IllegalStateException("The book with that ISBN is already in the database.");
+        }
+        bookRepository.save(book);
+    }
+
+    public void deleteBook(Long bookId) {
+        boolean exists = bookRepository.existsById(bookId);
+        if (!exists)
+        {
+            throw new IllegalStateException("Book with id " + bookId + "does not exist.");
+        }
+        bookRepository.deleteById(bookId);
     }
 }
