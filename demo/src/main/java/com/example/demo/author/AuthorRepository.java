fdfc430c2e5f97181e27851a9fd279fa1fb2c54c
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-03-03 17:18:40 -0500

    Create AuthorRepository.java

---
+++ demo/src/main/java/com/example/demo/author/AuthorRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.author;
+
+import com.example.demo.author.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findAuthorById(Long id);
+}
