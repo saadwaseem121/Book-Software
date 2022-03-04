0d9c083f19b9b8c3ce53115c725dceece974ee1e
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-03-04 13:05:37 -0500

    Create AuthorController.java

---
+++ demo/src/main/java/com/example/demo/author/AuthorController.java
@@ -0,0 +1,24 @@
+package com.example.demo.author;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/author")
+public class AuthorController
+{
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> getAuthors(){
+        return authorService.getAuthors();
+    }
+
+}
