aba90da9eb715c536494724d2749e0bd48ae96b4
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-03-04 13:06:06 -0500

    Create AuthorService.java

---
+++ demo/src/main/java/com/example/demo/author/AuthorService.java
@@ -0,0 +1,22 @@
+package com.example.demo.author;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Author> getAuthors() {
+        return authorRepository.findAll();
+    }
+}
