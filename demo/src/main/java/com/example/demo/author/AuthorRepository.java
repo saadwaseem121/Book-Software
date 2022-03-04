d33ece4ef9adeb301d1939f0428b9b6410014ee3
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-03-04 13:05:14 -0500

    Update AuthorRepository.java

--- demo/src/main/java/com/example/demo/author/AuthorRepository.java
+++ demo/src/main/java/com/example/demo/author/AuthorRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.author;
 
-import com.example.demo.author.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
