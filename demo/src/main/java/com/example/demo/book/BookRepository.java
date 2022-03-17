3f15d0225373f2267de8175277ad8f689e67a30b
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-03-16 20:12:45 -0400

    Update BookRepository.java

--- demo/src/main/java/com/example/demo/book/BookRepository.java
+++ demo/src/main/java/com/example/demo/book/BookRepository.java
@@ -18,5 +18,4 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 //    @Query("SELECT b FROM Book b JOIN author ON b.author_id = author.id WHERE b.author_id = ?1")
     List<Book> findBooksByAuthor_Id(Long author_id);
     List<Book> findBooksByGenre(String genre);
-
 }
