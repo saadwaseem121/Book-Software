b5ad9d196bc7658f2cfcb8c7195a5556105ed131
Kyle Maxwell <57728032+switch1e@users.noreply.github.com>
2022-03-17 13:21:38 -0400

    Create UserService.java

---
+++ demo/src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,26 @@
+package com.example.demo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+}
